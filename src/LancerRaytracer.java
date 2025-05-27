af00c75d74348f23711a746639e503e0f135b336
Valentxn7 <gugug3293@gmail.com>
2025-05-27 11:42:00 +0200

    add threading complet

--- src/LancerRaytracer.java
+++ src/LancerRaytracer.java
@@ -8,9 +8,27 @@ import raytracer.Image;
 
 import raytracer.Position;
 
-public class LancerRaytracer {
+public class LancerRaytracer implements ServiceRaytracing {
 
     public static String aide = "Raytracer : synthèse d'image par lancé de rayons (https://en.wikipedia.org/wiki/Ray_tracing_(graphics))\n\nUsage : java LancerRaytracer [fichier-scène] [largeur] [hauteur]\n\tfichier-scène : la description de la scène (par défaut simple.txt)\n\tlargeur : largeur de l'image calculée (par défaut 512)\n\thauteur : hauteur de l'image calculée (par défaut 512)\n";
+    public static Scene scene = null;
+    public static ArrayList<ServiceNoeud> noeuds = new ArrayList<>();
+
+    /**
+     * Récupère la scène à partir de laquelle le raytracing va être effectué.
+     */
+    public Scene getScene() {
+        return scene;
+    }
+
+    /**
+     * Enregistre un noeud dans le service de raytracing.
+     *
+     * @param noeud le noeud à enregistrer dans le service de raytracing.
+     */
+    public void enregistrerNoeud(ServiceNoeud noeud) {
+        noeuds.add(noeud);
+    }
 
     public static void main(String args[]) {
 
@@ -36,7 +54,7 @@ public class LancerRaytracer {
         Disp disp = new Disp("Raytracer", largeur, hauteur);
 
         // Initialisation d'une scène depuis le modèle 
-        Scene scene = new Scene(fichier_description, largeur, hauteur);
+        scene = new Scene(fichier_description, largeur, hauteur);
 
         // Calcul de l'image de la scène les paramètres : 
         // - x0 et y0 : correspondant au coin haut à gauche
@@ -62,22 +80,68 @@ public class LancerRaytracer {
             }
         }
 
+        ArrayList<ServiceNoeud> noeudsDisponible = new ArrayList<ServiceNoeud>(noeuds);
+
         // Chronométrage du temps de calcul
         Instant debut = Instant.now();
         System.out.println("Calcul de l'image :\n - Coordonnées : " + x0 + "," + y0
                 + "\n - Taille " + largeur + "x" + hauteur + "\n - Découpage : " + decoupage);
 
-        while(aCalculer.size() > 0) {
-            Position pos = aCalculer.get(0);
-            Image imagePart = scene.compute(pos.getX(), pos.getY(), decoupage, decoupage);
-            disp.setImage(imagePart, pos.getX(), pos.getY());
-            aCalculer.remove(pos);
+        ArrayList<Thread> threads = new ArrayList<>();
+
+        while (!aCalculer.isEmpty()) {
+
+            // local
+            // Position pos = aCalculer.get(0);
+            //Image imagePart = scene.compute(pos.getX(), pos.getY(), decoupage, decoupage);
+
+            // distant
+            if (noeudsDisponible.size() != 0) {
+                Position pos = aCalculer.get(0);
+
+                Thread t = new Thread(() -> {
+                    try {
+                        // On prend le premier noeud disponible pour calculer l'image
+                        ServiceNoeud noeud;
+                        synchronized (noeudsDisponible) {
+                            noeud = noeudsDisponible.remove(0);
+                        }
+
+                        Image imagePart = noeud.compute(pos.getX(), pos.getY(), decoupage, decoupage);
+
+                        synchronized (disp) {
+                            disp.setImage(imagePart, pos.getX(), pos.getY());
+                        }
+
+                        synchronized (noeudsDisponible) {
+                            noeudsDisponible.add(noeud);
+                        }
+
+                        synchronized (aCalculer) {
+                            aCalculer.remove(pos);
+                        }
+                    } catch (Exception e) {
+                        aCalculer.add(pos);
+                        e.printStackTrace();
+                    }
+                });
+                threads.add(t);
+                t.start();
+            }
+
+        }
+
+        // Attendre la fin de tous les threads
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         Instant fin = Instant.now();
-
         long duree = Duration.between(debut, fin).toMillis();
-
         System.out.println("Image calculée en :" + duree + " ms");
     }
 }
