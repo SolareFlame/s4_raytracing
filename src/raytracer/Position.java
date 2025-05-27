9f8a64ce4979762c7d140822ddc6c98dc937cadf
Valentxn7 <gugug3293@gmail.com>
2025-05-27 11:21:05 +0200

    add découpage, assemblage

---
+++ src/raytracer/Position.java
@@ -0,0 +1,27 @@
+package raytracer;
+
+public class Position {
+    private double x;
+    private double y;
+
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return (int) Math.round(x);
+    }
+
+    public int getY() {
+        return (int) Math.round(y);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
